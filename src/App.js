377c8dcb9451eb81e7db3eba054244ce4eb0bff0
drewkevich <AndrzejHK@gmail.com>
2022-08-12 16:50:03 +0200

    added search functionality

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
   const [isOpened, setIsOpened] = useState(false)
   const [items, setItems] = useState()
   const [cartItems, setCartItems] = useState([])
+  const [searchValue, setSearchValue] = useState("")
 
   const onOpenedCart = ()=>{
     setIsOpened(!isOpened)
@@ -60,16 +61,18 @@ function App() {
       <Header onOpenedCart={onOpenedCart}/>
       <div className="content p-30">
         <div className="d-flex justify-between align-center">
-          <h1>All T-shirts</h1>
+          <h1>{searchValue? `Result of search: ${searchValue}`:"All T-shirts"}</h1>
           <div className="search-block d-flex">
+            
             <img width={22} height={22} src="image/icons8-search-30.png" alt="Search"></img>
-            <input placeholder="Seach" type="search"></input>
+            <input onChange={(e)=>setSearchValue(e.target.value)} value={searchValue || "" } placeholder="Seach"></input>
+            {searchValue && <img className="clear cu-p" width={18} height={18} src="image/close.png" alt="Clear" onClick={()=>setSearchValue("")}></img>}
 
           </div>
         </div>
         
         <div className="d-flex flex-wrap">
-          {items&&items.map((item,index)=>(
+          {items && items.filter(item=>item.description.toLowerCase().includes(searchValue.toLowerCase())).map((item,index)=>(
               <Card key={index} items={item} onAddToCart={(obj)=>onAddToCart(obj)}></Card>
           )
           )}
