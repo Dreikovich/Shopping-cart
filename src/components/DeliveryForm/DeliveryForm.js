dc42db618a14b4e23975ea4142203ac17fb3c1fc
drewkevich <AndrzejHK@gmail.com>
2022-08-14 02:33:19 +0200

    added DeliveryForm component

---
+++ src/components/DeliveryForm/DeliveryForm.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import {useState, useContext} from 'react'
+import styles from "../DeliveryForm/DeliveryForm.module.scss"
+import axios from 'axios'
+import AppContext from '../../context'
+
+
+const DeliveryForm = () => {
+    const {cartItems} = useContext(AppContext) 
+    const [name, setName] = useState()
+    const [city, setCity] = useState()
+    const [phone, setPhone] = useState()
+    const [adress, setAdress] = useState()
+
+    const postData = async(obj)=>{
+        try{
+            await axios.post("https://62f615b0612c13062b45e6f7.mockapi.io/personData", obj)
+        }
+        catch(err){
+            console.log(err.message)
+        }
+    }
+
+    const getDataFromInput = (e) =>{
+        e.preventDefault()
+        const obj = {name, city, adress, phone, items:cartItems}
+        console.log(obj)
+        postData(obj)
+        //Your order is accepted
+
+    }
+
+  return (
+    <form onSubmit={getDataFromInput}>
+        <div className={styles.inputs}>
+            <div className={styles.input}>
+                <p>Name</p>   
+                <input placeholder="Enter your name..." onChange={(e)=>setName(e.target.value)}></input>
+            </div>
+            <div className={styles.input}>
+                <p>City</p>   
+                <input placeholder="Enter your city..." onChange={(e)=>setCity(e.target.value)}></input>
+            </div>
+            <div className={styles.input}>
+                <p>Adress</p>   
+                <input placeholder="Enter your adress..." onChange={(e)=>setAdress(e.target.value)}></input>
+            </div>
+            <div className={styles.input}>
+                <p>Phone</p>   
+                <input placeholder="Enter your phone..." onChange={(e)=>setPhone(e.target.value)}></input>
+            </div>
+            <button type="submit">Submit</button>
+        </div>
+    </form>
+    
+  )
+}
+
+export default DeliveryForm
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import AppContext from "../context"
 import {useContext} from 'react'
 import OrderItems from '../components/OrderItems/OrderItems'
+import DeliveryForm from '../components/DeliveryForm/DeliveryForm'
 const Checkout = () => {
     // const {cartItems} = useContext(AppContext)
   return (
     <div className="checkout p-40">
         <h3>Order details</h3>
         <OrderItems />
-        {/* <h3 style={{marginTop: '40px'}}>Delivery</h3> */}
-        {/* <DeliveryForm /> */}
+        <h3 style={{marginTop: '50px'}}>Delivery</h3>
+        <DeliveryForm />
     </div>
   )
 }
